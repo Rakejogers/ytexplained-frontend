72e0e38bd374459087a27cff84967bd866bd7dc2
Rakejogers <87666498+Rakejogers@users.noreply.github.com>
2023-07-23 02:41:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ async function fetch_explanation(){
     console.log(yt_id)
 
     try {
-        const response = await fetch('jakero.pythonanywhere.com/ytexplainer?yt_id=' + yt_id);
+        const response = await fetch('https://jakero.pythonanywhere.com/ytexplainer?yt_id=' + yt_id);
 
         console.log('status code: ', response.status); // 👉️ 200
         
